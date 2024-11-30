c4ad7b378ad3cc9f14598134bdf227e916a9917d
mariiiii0000 <lanat3939@gmail.com>
2024-11-30 18:06:26 +0100

    merry xmas

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -7,33 +7,40 @@ public class DepositCalculator {
     }
 
 
-    double calculateSimplePercent(double doubleAmount, double doubleYearRate, int deposit_period) {
+    double calculateSimplePercent(double doubleAmount, double yearRate, int deposit_period) {
         // Я в шоке, зачем нужнен "double" для названия переменных в параметре,прошу исправить данную проблему.Спасибо за понимание!
-        return round(doubleAmount + doubleAmount * doubleYearRate * deposit_period, 2);
+        return round(doubleAmount + doubleAmount * yearRate * deposit_period, 2);
     }
 
     double round(double value, int places) {
-        double ScaLe = Math.pow(10, places);
-        return Math.round(value * ScaLe) / ScaLe;
-        //Мария,я нахожусь в агонии, почему переменная названа с БОЛЬШОЙ БУКВЫ И ЛАТИНСКАЯ БУКВА "L" ТОЖЕ С БОЛЬШОЙ!прошу вас исправить эту проблему.Спасибо за понимание!Всех благ вам.Merry Xmas!
+        double scale = Math.pow(10, places);
+        return Math.round(value * scale) / scale;
+        //Мария,я нахожусь в агонии, почему переменная названа с БОЛЬШОЙ БУКВЫ И ЛАТИНСКАЯ БУКВА "L" ТОЖЕ С БОЛЬШОЙ!прошу
+        // вас исправить эту проблему.Спасибо за понимание!Всех благ вам.Merry Xmas!
     }
 
     void calculateTheResult() {
         int period, action;
         Scanner scanner = new Scanner(System.in); //О Боги мои!Название переменной просто божественна!Продолжайте в том же духе!
+
         System.out.println("Введите сумму вклада в рублях:");
         int amount = scanner.nextInt();
+
         System.out.println("Введите срок вклада в годах:");
         period = scanner.nextInt();
+
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = scanner.nextInt();
+
         double result = 0;
 
         if (action == 1) {
             result = calculateSimplePercent(amount, 0.06, period);
+
         } else if (action == 2) {
             result = calculateComplexPercent(amount, 0.06, period);
         }
+
         System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + result);
     }
     //Мария, вам не кажется,что этим строкам не хватает воздуха?Вот и мне также кажется.Прошу принять меры!Всех благ и благополучия!
